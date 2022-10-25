24af0bcc6bd9af451ac2c6cd95c86765f4747cb1
Sandro Antônio Souza <sandroantoniosouza98@gmail.com>
2022-10-25 09:15:45 -0300

    notificacao mixins

--- src/views/Projetos/Formulario.vue
+++ src/views/Projetos/Formulario.vue
@@ -20,8 +20,10 @@
 <script lang="ts">
 import { useStore } from '@/store'
 import { defineComponent } from 'vue'
-import { ALTERA_PROJETO, ADICIONA_PROJETO, NOTIFICAR } from '@/store/tipo-mutacoes'
+import { ALTERA_PROJETO, ADICIONA_PROJETO } from '@/store/tipo-mutacoes'
 import { TipoNotificacao } from '@/interfaces/INotificacao'
+import { notificacaoMixin } from '@/mixins/notificar'
+
 export default defineComponent({
   name: 'Formulario',
   props: {
@@ -29,6 +31,7 @@ export default defineComponent({
       type: String
     }
   },
+  mixins: [notificacaoMixin],
   mounted () {
     if(this.id) {
       const projeto = this.store.state.projetos.find(proj => proj.id == this.id)
@@ -51,11 +54,7 @@ export default defineComponent({
         this.store.commit(ADICIONA_PROJETO, this.nomeDoProjeto)
       }
       this.nomeDoProjeto = ''
-      this.store.commit(NOTIFICAR, {
-        titulo: 'Novo projeto foi salvo',
-        texto: 'Prontinho ;) seu projeto já está disponível.',
-        tipo: TipoNotificacao.SUCESSO
-      })
+      this.notificar(TipoNotificacao.SUCESSO, 'Excelente!', 'O projeto foi cadastrado com sucesso!')
       this.$router.push('/projetos')
     }
   },
